a26dcda4ce14f771a8218d2a3f60ee5b871ef641
Nimikin <shu.nik98@gmail.com>
2022-11-09 12:41:38 +0200

    File manager Training

--- src/main/java/com/knubisoft/command/My.java
+++ src/main/java/com/knubisoft/command/My.java
@@ -2,6 +2,7 @@ package src.main.java.com.knubisoft.command;
 
 public class My {
     public static void main(String[] args) {
-
+        System.out.println("K");
+        System.out.println();
     }
 }
